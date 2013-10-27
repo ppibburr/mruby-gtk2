dfe74378ee217612ca27b0e57d2df843f462995c
ppibburr <tulnor33@gmail.com>
2013-10-26 22:32:54 -0400

    add samples

--- samples/simple.rb
+++ samples/simple.rb
@@ -1,14 +1,12 @@
-Gtk::init()
+Gtk::init
 
-w = Gtk::Window.new Gtk::WindowType::TOPLEVEL
-w.set_title("simple.rb")
-
-w.add b = Gtk::Button.new_with_mnemonic("_Quit")
-
-b.signal_connect "clicked" do |widget, data|
-  Gtk::main_quit
-end
-
-w.show_all
-
+  w = Gtk::Window.new Gtk::WindowType::TOPLEVEL
+  w.add b = Gtk::Button.new_from_stock(Gtk::STOCK_QUIT)
+  
+  b.signal_connect "clicked" do |widget, data |
+    Gtk::main_quit
+  end
+  
+  w.show_all()
+  
 Gtk::main
