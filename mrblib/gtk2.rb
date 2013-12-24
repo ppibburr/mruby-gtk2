22bbc2c5ede57b9c2bbfa82e52504a697be6ae4d
ppibburr <tulnor33@gmail.com>
2013-12-23 19:28:26 -0500

    ...

--- mrblib/gtk2.rb
+++ mrblib/gtk2.rb
@@ -1,14 +1,4 @@
-## Explicit declaration to use version 2.x
-GirFFI::setup :Gtk, 2.0
-
-## TODO: any version 2 specific handling.
-## Gtk2 has Gtk::Object and must be loaded so as not to return ::Object
-##
-## GirFFI has a hook to detect version 2.x
-## and does this for us
-# Gtk.bind_class :Object,GirFFI::Data::make(GirFFI::REPO.find_by_name("Gtk","Object"))
-
-Gtk
+GirFFI::setup :Gtk,'2.0'
 
 module Gtk
   # we're overiding Gtk::init
@@ -30,4 +20,47 @@ module Gtk
     
     return _init_(0,nil)
   end
+  
+
+  self::Lib.attach_function :gtk_list_store_set,[:pointer,:pointer,:int,:pointer,:int],:void
+  self::Lib.attach_function :gtk_tree_model_get,[:pointer,:pointer,:int,:pointer,:int],:void
+  
+  Gtk::ListStore  
+  class ListStore
+    def set iter,col,v
+      v = GirFFI::coerce_pointer(v)
+      
+      iter = GirFFI.coerce_pointer(iter)
+      
+      Gtk::Lib.gtk_list_store_set self.to_ptr, iter.to_out(true), col, v,-1
+    end
+    
+    def self.new *o
+      self.newv *o
+    end
+  end
+  
+  module TreeModel
+    def get iter,col
+      iter = GirFFI::coerce_pointer(iter)
+      v = FFI::MemoryPointer.new :pointer
+      type = get_column_type(col)    
+      Gtk::Lib.gtk_tree_model_get self.to_ptr, iter.to_out(true), col, v.to_out(true), -1
+      return GirFFI::gtype_value2ruby(type,v)
+    end
+    
+    bind_instance_method :get_value, i=find_function(:get_value)
+    def get_value iter,col
+      iter = GirFFI::coerce_pointer(iter)
+      val = GObject::Value.new
+      
+      # BUG: bug in mruby likely... `g_value_init` from libgobject-2.0 will segfault
+      #      so we init to the proper type ourselves, only throws a warning
+      val.init get_column_type(col)
+      
+      Gtk::Lib.gtk_tree_model_get_value(self.to_ptr,iter.to_out(true),col,val.to_ptr)
+      return val.get_string
+    end
+  end
 end
+
